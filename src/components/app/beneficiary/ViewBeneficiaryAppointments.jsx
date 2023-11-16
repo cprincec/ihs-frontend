c79b6de75bec3ba34410f328f07dbebfc6fc84ca
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2023-11-16 10:13:46 +0100

    refactor beneficiaries appointment tables

--- src/components/app/beneficiary/ViewBeneficiaryAppointments.jsx
+++ src/components/app/beneficiary/ViewBeneficiaryAppointments.jsx
@@ -5,85 +5,88 @@ import UpcomingAppointmentsTable from "../appointment/UpcomingAppointmentsTable"
 import CompletedAppointmentsTable from "../appointment/CompletedAppointmentsTable";
 import useFetch from "../../../hooks/useFetch";
 import { useState } from "react";
+import AppointmentTable from "../appointment/AppointmentTable";
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(" ");
 }
 
 TopBarProgress.config({
-    barColors: {
-        0: "#05afb0",
-    },
-    shadowBlur: 5,
+  barColors: {
+    0: "#05afb0",
+  },
+  shadowBlur: 5,
 });
 
 const ViewBeneficiaryAppointments = () => {
-    const params = useParams();
-    const [errMsg, setErrMsg] = useState("");
-    const { isSuccess, isLoading, data, isError, error } = useFetch(
-        `/user/appointments/beneficiary/${params.beneficiaryId}`,
-        `appointments, ${params.beneficiaryId}`
-    );
+  const params = useParams();
+  const [errMsg, setErrMsg] = useState("");
+  const { isSuccess, isLoading, data, isError, error } = useFetch(
+    `/user/appointments/beneficiary/${params.beneficiaryId}`,
+    `appointments, ${params.beneficiaryId}`
+  );
 
-    return (
-        <div className="w-full px-2 pb-16 sm:px-0">
-            <p
-                className={
-                    errMsg
-                        ? "rounded-md p-4 mb-4 bg-ihs-green-shade-200 text-red-500 font-normal text-lg"
-                        : "absolute -left-[99999px]"
-                }
-                aria-live="assertive"
-            >
-                {errMsg}
-            </p>
+  return (
+    <div className="w-full px-2 pb-16 sm:px-0">
+      <p
+        className={
+          errMsg
+            ? "rounded-md p-4 mb-4 bg-ihs-green-shade-200 text-red-500 font-normal text-lg"
+            : "absolute -left-[99999px]"
+        }
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
 
-            {isLoading && <TopBarProgress />}
-            {isError && setErrMsg(error)}
-            {isSuccess && (
-                <Tab.Group>
-                    <Tab.List className="flex space-x-1 rounded-md bg-gray-200 p-2 md:w-1/2">
-                        <Tab
-                            key="upcoming"
-                            className={({ selected }) =>
-                                classNames(
-                                    "w-full rounded-md py-2.5 text-sm leading-5 border-0 outline-none",
-                                    selected
-                                        ? "bg-ihs-green text-white hover:bg-ihs-green hover:text-white"
-                                        : "text-gray-500 hover:text-gray-500"
-                                )
-                            }
-                        >
-                            Upcoming
-                        </Tab>
-                        <Tab
-                            key="completed"
-                            className={({ selected }) =>
-                                classNames(
-                                    "w-full rounded-md py-2.5 text-sm leading-5 border-0 outline-none",
-                                    selected
-                                        ? "bg-ihs-green text-white hover:bg-ihs-green hover:text-white"
-                                        : "text-gray-500 hover:text-gray-500"
-                                )
-                            }
-                        >
-                            Completed
-                        </Tab>
-                    </Tab.List>
+      {isLoading && <TopBarProgress />}
+      {isError && setErrMsg(error)}
+      {isSuccess && (
+        <AppointmentTable appointments={data} />
+        // <Tab.Group>
+        //     <Tab.List className="flex space-x-1 rounded-md bg-gray-200 p-2 md:w-1/2">
+        //         <Tab
+        //             key="upcoming"
+        //             className={({ selected }) =>
+        //                 classNames(
+        //                     "w-full rounded-md py-2.5 text-sm leading-5 border-0 outline-none",
+        //                     selected
+        //                         ? "bg-ihs-green text-white hover:bg-ihs-green hover:text-white"
+        //                         : "text-gray-500 hover:text-gray-500"
+        //                 )
+        //             }
+        //         >
+        //             Upcoming
+        //         </Tab>
+        //         <Tab
+        //             key="completed"
+        //             className={({ selected }) =>
+        //                 classNames(
+        //                     "w-full rounded-md py-2.5 text-sm leading-5 border-0 outline-none",
+        //                     selected
+        //                         ? "bg-ihs-green text-white hover:bg-ihs-green hover:text-white"
+        //                         : "text-gray-500 hover:text-gray-500"
+        //                 )
+        //             }
+        //         >
+        //             Completed
+        //         </Tab>
+        //     </Tab.List>
 
-                    <Tab.Panels>
-                        <Tab.Panel>
-                            <UpcomingAppointmentsTable appointmentList={data} urlPath="appointments" />
-                        </Tab.Panel>
+        //     <Tab.Panels>
+        //         <Tab.Panel>
 
-                        <Tab.Panel>
-                            <CompletedAppointmentsTable appointmentList={data} urlPath="appointments" />
-                        </Tab.Panel>
-                    </Tab.Panels>
-                </Tab.Group>
-            )}
-        </div>
-    );
+        //             <UpcomingAppointmentsTable appointmentList={data} urlPath="appointments" />
+        //         </Tab.Panel>
+
+        //         <Tab.Panel>
+        //             <CompletedAppointmentsTable appointmentList={data} urlPath="appointments" />
+        //         </Tab.Panel>
+        //     </Tab.Panels>
+        // </Tab.Group>
+      )}
+    </div>
+  );
 };
 
 export default ViewBeneficiaryAppointments;
